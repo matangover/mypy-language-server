7026d8c5e14f3a2115e564a326c3004564fda027
Matan Gover <matangover@gmail.com>
2019-05-13 22:09:27 -0400

    Improve hover content and formatting.

--- mypyls/mypy_hover.py
+++ mypyls/mypy_hover.py
@@ -11,6 +11,7 @@ from mypy.types import (
 from mypy.util import short_type
 from .mypy_definition import get_import_definition
 from . import mypy_utils
+import re
 
 log = logging.getLogger(__name__)
 
@@ -20,7 +21,12 @@ def hover(workspace, document, position):
         return None
     hover = get_hover(fgmanager, document.path, position['line'], position['character'])
     if hover:
-        return {'contents': hover}
+        return {
+            'contents': {
+                'kind': 'markdown',
+                'value': python_highlight(hover)
+            }
+        }
     else:
         return None
 
@@ -40,6 +46,8 @@ def get_hover(fgmanager, path, line, column) -> Optional[str]:
 
     def_node: Optional[Node] = None
     if isinstance(node, NameExpr):
+        if node.fullname == 'builtins.None':
+            return None
         def_node = node.node
     elif isinstance(node, Instance):
         def_node = node.type
@@ -52,25 +60,41 @@ def get_hover(fgmanager, path, line, column) -> Optional[str]:
         return None
 
     if isinstance(def_node, Var):
-        var_type = 'Unknown' if def_node.type is None else str(def_node.type)
+        var_type = 'Unknown' if def_node.type is None else type_to_string(def_node.type)
         return f'{def_node.name()}: {var_type}'
 
-        # if isinstance(def_node.type, AnyType):
-        #     return 'Any'
-        # if isinstance(def_node.type, Instance):
-        #     return def_node.type.type.fullname()
-        # return short_type(def_node.type)
-
     if isinstance(def_node, TypeInfo):
-        return def_node.fullname()
+        return f'class {fullname(def_node)}'
 
     if isinstance(def_node, MypyFile):
-        return f'{def_node.fullname()}: module'
+        return f'{def_node.fullname()} (module)'
 
     if isinstance(def_node, FuncBase):
-        result = f'function {def_node.fullname()}'
-        if isinstance(def_node, FuncDef):
-            result += f'({", ".join(def_node.arg_names)})'
-        return result
+        type_str = ''
+        node_type = fgmanager.manager.all_types.get(node) or def_node.type
+        if node_type:
+            type_str = type_to_string(node_type)
+            if type_str.startswith('def '):
+                type_str = type_str[4:]
+        return fullname(def_node) + type_str
 
     return None
+
+def python_highlight(value):
+    return f'```python\n{value}\n```'
+
+def type_to_string(typ: Type) -> str:
+    type_str = str(typ)
+    # Strip any occurrence of 'builtins.' unless it's part of an identifier.
+    # (e.g. don't strip some_builtins.blah or a.builtins.hello )
+    # This might miss some cases with Unicode identifiers, but it's
+    # sufficiently rare to have a type called שלום_builtins.טירוף.
+    # https://docs.python.org/3/reference/lexical_analysis.html#identifiers
+    return re.sub(r'([^.a-zA-Z0-9_]|^)builtins\.', r'\1', type_str)
+
+def fullname(node: SymbolNode) -> str:
+    name = node.fullname()
+    if name.startswith('builtins.'):
+        return name[9:]
+    else:
+        return name
